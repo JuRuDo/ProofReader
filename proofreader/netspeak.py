218c828e88c10240b6912ca9e67848386d203a2f
Julian Dosch <43743618+jurudo@users.noreply.github.com>
2019-03-11 16:04:16 +0100

    Day 1 finish

--- proofreader/netspeak.py
+++ proofreader/netspeak.py
@@ -1,15 +1,24 @@
 #!/bin/env python
 
+from urllib import parse, request
+
+NETSPEAK_REST_BASE_URL = "http://api.netspeak.org/netspeak3/search"
+
+
 class Netspeak:
     _base_query_url = "http://api.netspeak.org/netspeak3/search"
 
     def __call__(self, user_input):
         encoded_query = self._encode_query(user_input)
-        self._make_request(encoded_query)
-        pass
+        result = self._make_request(encoded_query)
+        return self._read_result(result)
 
     def _encode_query(self, query):
-        pass
+        parameters = {"query": query, "format": "text"}
+        return parse.urlencode(parameters)
 
     def _make_request(self, encoded_query):
-        pass
+        return request.urlopen(self._base_query_url + "?" + encoded_query)
+
+    def _read_result(self, result):
+        return result.read().decode()
