158e69b7f8d6e110dbc18f46a624f37d83b879ed
Julian <juliandosch@web.de>
2019-03-12 12:00:09 +0100

    update license for all modules

--- setup.py
+++ setup.py
@@ -1,14 +1,50 @@
+#!/bin/env python
+
+#######################################################################
+# Copyright (C) 2019 Julian Dosch
+#
+# This file is part of ProofReader.
+#
+#  ProofReader is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  ProofReader is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with ProofReader.  If not, see <http://www.gnu.org/licenses/>.
+#
+#######################################################################
+
 from setuptools import setup, find_packages
+
+with open("README.md", "r") as input:
+    long_description = input.read()
 
 setup(
     name="ProofReader",
     version="1.0.0",
     description="A proofreader of English texts",
+    long_description= long_description,
     author="Julian Dosch",
     author_email="Dosch@bio.uni-frankfurt.de",
+    url = "https://github.com/JuRuDo/ProofReader",
     packages=find_packages(),
+    install_requires=[],
     entry_points={
         'console_scripts': ["proofread = proofreader.proofread:main"],
     },
     license="GPL-3.0",
+    classifiers=[
+        "Environment :: Console",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+    ],
 )
